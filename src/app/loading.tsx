026b4e7e5770b9dc0259d5c3ffdb0f6aa9db0744
Xpektra <xpektra7@gmail.com>
2025-10-25 01:59:21 +0100

    update: restyled loading page for home route

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,10 @@
+import { Loader } from "lucide-react";
+
 export default function Loading() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1 className="text-4xl font-bold">Loading...</h1>
+      <Loader className="animate-spin size-8 text-foreground" />
     </div>
   );
 }
