777928f599c177f83d2076ef18b69224d24f9a56
Xpektra <xpektra7@gmail.com>
2025-10-25 01:59:50 +0100

    style: enhance hero section with improved heading size and button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,7 @@ export default function Home() {
       <main className="flex flex-col gap-24">
         <section id="hero" className="flex flex-col gap-4">
           <div className="flex justify-between items-center">
-            <h1 className="text-foreground font-semibold">Hey, I’m <span className="text-chart-4">Xpektra</span>.</h1>
-            <Button variant="outline" size="sm" asChild>
-              <Link href="/resume.pdf" target="_blank" rel="noopener noreferrer">
-                Resume
-                <ArrowDownToLine />
-              </Link>
-            </Button>
+            <h1 className="text-foreground text-2xl font-semibold">Hey, I’m <span className="text-chart-4">Xpektra</span>.</h1>
           </div>
 
           <p className="text-sm">I’m a frontend and embedded systems engineer who thrives at the intersection of hardware and software. I design and build systems that connect the physical and digital, from intelligent traffic prototypes to seamless web interfaces.
@@ -25,29 +19,12 @@ export default function Home() {
             <br />
             My work blends precision engineering with creative design thinking. I turn abstract ideas into high performance solutions, whether optimizing sensor data flows or refining UI motion down to a single pixel.
           </p>
-          <div className="flex items-center text-foreground gap-2">
-            <Button variant="secondary" size="icon-sm" asChild aria-label="Link to my Github account">
-              <Link href="https://github.com/Xpektra7" target="_blank" rel="noopener noreferrer">
-                <Github />
+            <Button variant="outline" size="sm" asChild className="w-fit">
+              <Link href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+                Resume
+                <ArrowDownToLine />
               </Link>
             </Button>
-            <Button variant="secondary" size="icon-sm" asChild aria-label="Link to my Twitter account">
-              <Link href="https://www.x.com/xpektra7" target="_blank" rel="noopener noreferrer">
-                <TwitterIcon />
-              </Link>
-            </Button>
-            <Button variant="secondary" size="icon-sm" asChild aria-label="Link to my Linkedin account">
-              <Link href="https://www.linkedin.com/in/xpektra" target="_blank" rel="noopener noreferrer">
-                <LinkedinIcon/>
-              </Link>
-            </Button>
-            <Button variant="secondary" size="icon-sm" asChild aria-label="Send me an Email">
-              <Link href="https://www.linkedin.com/in/xpektra" target="_blank" rel="noopener noreferrer">
-                <MailIcon />
-              </Link>
-            </Button>
-          </div>
-
         </section>
         <Projects limit={3} />
       </main>
