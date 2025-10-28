2342739b845bde9b18dc71dc995a92682a4a1147
Xpektra <xpektra7@gmail.com>
2025-10-28 02:06:05 +0100

    testing image upload

---
+++ public/editorData.js
@@ -0,0 +1,41 @@
+export const data = {
+  time: 1761613158449,
+  blocks: [
+    {
+      id: "9yyQguZ5H3",
+      type: "header",
+      data: {
+        text: "How i coded an award winning masterpiece",
+        level: 1,
+      },
+    },
+    {
+      id: "FNHkG_gew4",
+      type: "image",
+      data: {
+        caption: "Cypher Website",
+        withBorder: true,
+        withBackground: false,
+        stretched: true,
+        file: {
+          url: "/uploads/1761612963045-Screenshot_20251003_124835.png",
+        },
+      },
+    },
+    {
+      id: "rreUat5jTp",
+      type: "paragraph",
+      data: {
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi placeat beatae iusto, obcaecati asperiores veniam consequuntur iure ipsam et aperiam quidem corrupti corporis minus quisquam error. Explicabo dolorem quisquam suscipit illo vel porro, inventore asperiores deleniti consequatur. At, aperiam ducimus.",
+      },
+    },
+    {
+      id: "5Z9Ar7GHuD",
+      type: "paragraph",
+      data: {
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi placeat beatae iusto, obcaecati asperiores veniam consequuntur iure ipsam et aperiam quidem corrupti corporis minus quisquam error. Explicabo dolorem quisquam suscipit illo vel porro, inventore asperiores deleniti consequatur. At, aperiam ducimus.",
+      },
+    },
+  ],
+  version: "2.31.0",
+};
