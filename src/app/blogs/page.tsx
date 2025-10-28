d23bb9085c449a1bfac3b4b9d6dc9c068fcd6f54
Xpektra <xpektra7@gmail.com>
2025-10-28 12:15:20 +0100

    updating blog page ui

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,6 +2,11 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/firebase"; // your firebase.ts file
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import Error from "./error";
+import Loading from "./loading";
 
 interface Blog {
   id: string;
@@ -12,32 +17,62 @@ interface Blog {
 
 export default function BlogsList() {
   const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchBlogs = async () => {
-      const querySnapshot = await getDocs(collection(db, "blogs"));
-      const blogData = querySnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      })) as Blog[];
-      setBlogs(blogData);
+      try {
+        const querySnapshot = await getDocs(collection(db, "blogs"));
+        const blogData = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Blog[];
+        setBlogs(blogData);
+      } catch {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
     };
     fetchBlogs();
   }, []);
 
+  if (error) return <Error />;
+
   return (
-    <div className="grid gap-4 ">
-      {blogs.map((blog) => (
-        <div key={blog.id} className="flex flex-col gap-2 border p-4 rounded">
-          <div className="flex justify-between gap-2">
-            <h2 className="text-xs md:text-sm font-bold ">{blog.title}</h2>
-            <p className="text-xs">{blog.createdAt?.toDate().toLocaleDateString() || "Unknown date"}</p>
-          </div>
-          <p className="text-xs line-clamp-1">
-            {blog.content?.blocks?.[1]?.data?.text || "No preview available"}
-          </p>
-        </div>
-      ))}
+    <div className="flex flex-col w-full">
+      <>
+        <Button variant="link" className="text-xs self-start p-0" asChild>
+          <Link href="/">
+            <ArrowLeft className="size-4" />
+            Back to Top
+          </Link>
+        </Button>
+        <hr />
+      </>
+
+      <div className="flex justify-between items-center mt-8 mb-4">
+        <h1 className="text-xl text-foreground font-bold">
+          Blogs
+        </h1>
+      </div>
+
+      {!loading ? (
+        <div className="grid gap-4">
+          {blogs.map((blog) => (
+            <div key={blog.id} className="flex flex-col gap-2 border p-4 rounded">
+              <div className="flex justify-between gap-2">
+                <h2 className="text-xs md:text-sm font-bold ">{blog.title}</h2>
+                <p className="text-xs">{blog.createdAt?.toDate().toLocaleDateString() || "Unknown date"}</p>
+              </div>
+              <p className="text-xs line-clamp-1">
+                {blog.content?.blocks?.[1]?.data?.text || "No preview available"}
+              </p>
+            </div>
+          ))}
+        </div>) : <Loading />}
+
     </div>
   );
 }
